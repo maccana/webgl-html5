bad5a2b9d2564a3650ca4eefc745caec54a97c61
maccana <mikeymccann@gmail.com>
2016-08-17 09:33:59 -0400

    Update app.js

--- webGL/app.js
+++ webGL/app.js
@@ -1,18 +1,19 @@
-var InitDemo = function(){
-	console.log('app.js loaded and InitDemo called...')
+window.onload = function InitDemo() {
+    console.log('app.js loaded and InitDemo called...')
 
-	var canvas = document.getElementById("game-surface");
-	var gl = canvas.getContext('webgl');
+    var canvas = document.getElementById("game-surface");
+    var gl = canvas.getContext('webgl');
 
-	if(!gl){
-		console.log('WebGl not supported.Falling back on experimental-webgl');
-		gl = canvas.getContext('experimental-webgl');
-	}
-	if(!gl){
-		alert('Your browser does not support WebGL');
-	}
+    if (!gl) {
+        console.log('WebGl not supported.Falling back on experimental-webgl');
+        gl = canvas.getContext('experimental-webgl');
+    }
+    if (!gl) {
+        alert('Your browser does not support WebGL');
+    }
 
-	gl.clearColor(0.75, 0.85, 0.8, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 }
+
