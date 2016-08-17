3ba76340bd8b4666228c52bacad1be3483fcd7f0
maccana <mikeymccann@gmail.com>
2016-08-16 23:03:30 -0400

    first commit

---
+++ webGL/app.js
@@ -0,0 +1,18 @@
+var InitDemo = function(){
+	console.log('app.js loaded and InitDemo called...')
+
+	var canvas = document.getElementById("game-surface");
+	var gl = canvas.getContext('webgl');
+
+	if(!gl){
+		console.log('WebGl not supported.Falling back on experimental-webgl');
+		gl = canvas.getContext('experimental-webgl');
+	}
+	if(!gl){
+		alert('Your browser does not support WebGL');
+	}
+
+	gl.clearColor(0.75, 0.85, 0.8, 1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+}
